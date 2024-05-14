29cd9ff09ce9a08ccc2515c3f5c8fa1655a2aee5
𝐉𝐀𝐂𝐊𝐈𝐌 𝐃𝐄𝐕𝐒 <167577317+Jackimcode10@users.noreply.github.com>
2024-05-14 23:44:40 +0300

    Update Tempmails.js

--- commands/Tempmails.js
+++ commands/Tempmails.js
@@ -20,10 +20,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { france } = require("../framework/france");
 france({ nomCom: "tempmail", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*FLASH-MD* Temporary emails,Powered by *Mc Jackim* \n\n ' + "Click the links below to create unlimited TEMPORARY Emails. Powered by *JACKIM-MD.*\n\n";
-    let d = ' 1️⃣ https://tempumail.com\n\n 2️⃣ https://etempmail.com\n\n 3️⃣ https://ghostmail.one\n\n 4️⃣ https://tempmailid.com';
+    let z = '*JACKIM-MD* Temporary emails,Powered by *Mc Jackim* \n\n ' + "Click the links below to create unlimited TEMPORARY Emails. Powered by *JACKIM-MD.*\n\n";
+    let d = ' Ⓜ https://tempumail.com\n\n Ⓜ https://etempmail.com\n\n Ⓜ https://ghostmail.one\n\n Ⓜ https://tempmailid.com';
     let varmess = z + d;
-    var img = 'https://telegra.ph/file/f1f2303ff4e39b0a3b6b3.jpg';
+    var img = 'https://telegra.ph/file/d46782633470af2059c33.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
     //console.log("montest")
 });
